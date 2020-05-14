5d0d68ddd9d8d0fc28d4fed6bba09f12225f3aa0
Jonathan-Gore <43797248+Jonathan-Gore@users.noreply.github.com>
2020-05-13 22:48:48 -0600

    Restructured Directory structure for future development
    Created Image comparision tool, change data_input.py to util.py

--- main/util.py
+++ main/util.py
@@ -2,6 +2,7 @@ from PIL import Image
 import os
 import sys
 import numpy as np
+import itertools
 
 from skimage.metrics import structural_similarity as ssim
 from skimage.color import rgb2gray
@@ -11,7 +12,9 @@ from skimage.io import imread
 #Grabs the current working directory and moves up once into the parent directory
 #This allows us to write to a temp folder, later will become important, also looks cleaner
 directory = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+directory1 = "Z:\Datasets\Apples\downloads\Applefruit"
 
+<<<<<<< Updated upstream
 #im1 = Image.open(sys.argv[1])
 #im1 = Image.open("C:/git/applebottomgenius/temp/rotate_temp/rotate180degrees.jpg")
 im1 = Image.open("C:/git/applebottomgenius/temp/rotate_temp/rotate180degrees.jpg")
@@ -24,6 +27,11 @@ im2 = Image.open("C:/git/applebottomgenius/images/jpg/apple.jpg")
 
 #im1_array = np.array(im1)
 #print(im1_array.shape)
+=======
+
+# im1= Image.open("Z:\\Datasets\\Apples\\downloads\\Applefruit\\compare\\green.jpg")
+# im2= Image.open("Z:\\Datasets\\Apples\\downloads\\Applefruit\\compare\\red.jpg")
+>>>>>>> Stashed changes
 
 
 #global color variables
@@ -41,8 +49,6 @@ def cleanImage(dirty_image):
     else:
         print("What the heck is this? A TIF? geez, come back with a JPG or a PNG when you are serious about this")
         return
-
-#cleanImage(im1)
 
 
 #needs to updating to conform with William's pillowtest.py (much better/flexible)
@@ -95,33 +101,80 @@ def rotateImage(image1):
     #    print("Resultant dictionary is : " + str(rotateDict[y]))
     #    y += 1
 
+<<<<<<< Updated upstream
 #rotateImage(im1)
+=======
+#rotateImage(im1, 11)
+>>>>>>> Stashed changes
 
 #uses SSIM to compare two images of the same size
 def compareImages(image1, image2):
 
-    #image1 = Image.open("C:/git/applebottomgenius/temp/rotate_temp/rotate1degrees.jpg")
-    #image1 = Image.open("C:/git/applebottomgenius/images/jpg/apple.jpg")
-    #image2 = Image.open("C:/git/applebottomgenius/images/jpg/Honeycrisp.jpg")
-
-    # image1.save('C:/git/applebottomgenius/temp/SSIMa.jpg')
-    # image2.save('C:/git/applebottomgenius/temp/SSIMb.jpg')
-    #
-    # image1a = Image.open('C:/git/applebottomgenius/temp/SSIMa.jpg')
-    # image2a = Image.open('C:/git/applebottomgenius/temp/SSIMb.jpg')
-
     image1a = np.array(image1.resize(image2.size))
     image2a = np.array(image2)
 
-    #ssim can be used with color, but it is a massively more complex process for our needs, so we convert to grayscale
-    rgb2gray(image1a)
-    rgb2gray(image2a)
-    structural_similarity = ssim(image1a, image2a, multichannel=True, gaussian_weights=True, sigma=1.5, use_sample_covariance=False, data_range=1.0)
+    #ssim(compare_image, base_image, data_range={What range to compare}
+    structural_similarity = ssim(image1a, image2a, multichannel=True, data_range=image2a.max() - image2a.min())
+    return structural_similarity
 
-    print(structural_similarity)
+##allowing for variables to return values outside of function
+# result = compareImages(im1, im2)
+# print("printing compareImages function value after running it: " + str(result))
 
+#compares all images within a directory and writes a copylist.txt with every duplicate pair
+def compareALLImages(file_directory):
+
+    files = os.listdir(file_directory)
+    similarlist = []
+
+    # concatenate directory into file list
+    for i in range(len(files)):
+        files[i] = str(file_directory + "\\" + files[i])
+
+    index = range(len(files))
+    for a, b in itertools.combinations(index, 2):
+        image1 = Image.open(files[a])
+        image2 = Image.open(files[b])
+
+        image1a = np.array(image1.resize(image2.size))
+        image2a = np.array(image2)
+
+        print("Comparing image " + str(a) + " " + "vs image " + str(b))
+        structural_similarity = ssim(image1a, image2a, multichannel=True, data_range=image2a.max() - image2a.min())
+
+        image1.close()
+        image2.close()
+
+        if structural_similarity >= 0.7:
+            print("Completed Comparison: !!WARNING!! ")
+            print("Images are >= 70% similar, adding both images to similarlist.txt")
+            print(" ")
+            similarlist.append(files[a])
+            similarlist.append(files[b])
+        elif structural_similarity < 1:
+            print("Completed comparison: Images are sufficiently unique")
+            print(" ")
+
+<<<<<<< Updated upstream
 compareImages(im1, im2)
+=======
+    with open(file_directory + "\\" + 'similarlist.txt', 'w') as filehandle:
+        for similarimage in similarlist:
+            filehandle.write('%s\n' % similarimage)
+>>>>>>> Stashed changes
 
+    return similarlist
+
+results = compareALLImages(directory1)
+print(results)
+
+# a_list = ("a", "b", "c", "d", "e")
+#
+# for a, b in itertools.combinations(a_list, 2):
+#     if a == b:
+#         print("a is equal to b, need to print this combination")
+#     else:
+#         print(a, b)
 
 def compareImagesLoop(image1, image2):
 
@@ -142,7 +195,7 @@ def compareImagesLoop(image1, image2):
         #ssim can be used with color, but it is a massively more complex process for our needs, so we convert to grayscale
         rgb2gray(image1a)
         rgb2gray(image2a)
-        structural_similarity = ssim(image1a, image2a, multichannel=True, gaussian_weights=True, sigma=1.5, use_sample_covariance=False, data_range=1.0)
+        structural_similarity = ssim(image1a, image2a, multichannel=True, data_range=image2a.max() - image2a.min())
         print(structural_similarity)
         ssimScores = {i,structural_similarity}
         print(ssimScores)
